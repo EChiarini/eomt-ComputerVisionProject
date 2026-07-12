48d1f34409f44928b7c6e364646cfc0650b6fd06
Emiliano chiarini <emiliano40017@gmail.com>
2026-07-12 10:18:23 +0200

    updated init

--- finetuning/__init__.py
+++ finetuning/__init__.py
@@ -1,6 +1,9 @@
 # All the utils that will be used for the finetuning part of the project.
-#
+
 # Consists of:
-#   loveda      - constants of the dataset (classi, palette, download)
-#   download    - download di dataset e checkpoint
-#   visualize   - to do
+#   loveda      - constants of the dataset (classes, palette, download)
+#   coco_classes- COCO panoptic classes and COCO -> LoveDA mapping
+#   download    - download of the dataset and the checkpoint
+#   evaluator   - model construction, weight loading, mIoU evaluation
+#   visualize   - qualitative visualizations (GT vs predictions)
+#   semantic_module - previews on disk
